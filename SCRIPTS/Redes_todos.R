53dce46d4bbe223c7a32a235d18dd346650fd2c7
mtz-mel <amartinez445@alumnos.uaq.mx>
2025-05-25 11:13:25 -0600

    todos

--- SCRIPTS/Redes_todos.R
+++ SCRIPTS/Redes_todos.R
@@ -1,5 +1,5 @@
 ## Redes de co-ocurrencia con y sin materia obscura, tomando en cuenta todos los
-## datos de SprockettTHData, los cuales, viven como objeto phyloseq en physeq_spp
+## datos de SprockettTHData, los cuales, viven como objeto phyloseq en physeq_especie
 
 ## Procedimiento: 
 
@@ -15,43 +15,62 @@ library(phyloseq)
 library(SpiecEasi)
 
 # CARGA DE DATOS
-dataspo <- SprockettTHData()
+datos_sprockett <- SprockettTHData()
+
+ncol(datos_sprockett)
 
 # MATRICES
-otu <- assays(dataspo)$counts %>% as.matrix()
-tax <- rowData(dataspo) %>% as.data.frame() %>% as.matrix()
-sample_data_df <- colData(dataspo) %>% as.data.frame()
+matriz_otu <- assays(datos_sprockett)$counts %>% as.matrix()
+matriz_tax <- rowData(datos_sprockett) %>% as.data.frame() %>% as.matrix()
+datos_muestras <- colData(datos_sprockett) %>% as.data.frame()
 
-# CONSTRUCCIÓN DE PHYLOSEQ
-OTU     <- otu_table(otu, taxa_are_rows = TRUE)
-TAX     <- tax_table(tax)
-SAMPLES <- sample_data(sample_data_df)
-physeq <- phyloseq(OTU, TAX, SAMPLES)
+# CONSTRUCCIÓN DE OBJETO PHYLOSEQ
+OTU <- otu_table(matriz_otu, taxa_are_rows = TRUE)
+TAX <- tax_table(matriz_tax)
+MUESTRAS <- sample_data(datos_muestras)
+physeq <- phyloseq(OTU, TAX, MUESTRAS)
 
 # AGRUPACIÓN A NIVEL DE ESPECIE (INCLUYENDO NA)
-physeq_spp <- tax_glom(physeq, taxrank = "Species", NArm = FALSE)
+physeq_especie <- tax_glom(physeq, taxrank = "Species", NArm = FALSE)
 
-# DIVIDIR physeq_spp
+# DIVISIÓN SEGÚN PRESENCIA DE MATERIA OSCURA
+physeq_conocido <- subset_taxa(physeq_especie, !is.na(Species))  # sin materia obscura
+physeq_todos <- physeq_especie  # con materia obscura
 
-physeq_known     <- subset_taxa(physeq_spp, !is.na(Species))  # sin tomar en cuenta la materia oscura
-physeq_all       <- physeq_spp  # tomando en cuenta la materia oscura
-
-
-# FILTRADO DE PREVALENCIA
-
-prevalence_filter <- function(phy, threshold = 0.2) {
-  prev <- apply(otu_table(phy), 1, function(x) mean(x > 0))
-  keep <- names(prev[prev >= threshold])
-  prune_taxa(keep, phy)
+# FUNCIÓN DE FILTRADO POR PREVALENCIA
+filtrar_prevalencia <- function(physeq_objeto, umbral = 0.1) {
+  prevalencia <- apply(otu_table(physeq_objeto), 1, function(x) mean(x > 0))
+  conservar <- names(prevalencia[prevalencia >= umbral])
+  prune_taxa(conservar, physeq_objeto)
 }
 
-physeq_known_filt <- prevalence_filter(physeq_known, 0.2)
-physeq_all_filt   <- prevalence_filter(physeq_all, 0.2)
+physeq_conocido_filtrado <- filtrar_prevalencia(physeq_conocido, 0.1)
+physeq_todos_filtrado    <- filtrar_prevalencia(physeq_todos, 0.1)
 
-# REDES SPIEC-EASI
+# Renombrar los taxa en physeq_especie (aplicable a physeq_todos y physeq_conocido)
 
-se_known <- spiec.easi(
-  physeq_known_filt,
+renombrar_especies <- function(physeq_objeto) {
+  tax <- tax_table(physeq_objeto)
+  especies <- as.character(tax[, "Species"])
+  
+  # Reemplazar NA con "Desconocido"
+  especies_limpias <- ifelse(is.na(especies), "Desconocido", especies)
+  
+  # Asegurar unicidad en los nombres
+  nombres_unicos <- make.unique(especies_limpias)
+  
+  # Asignar los nuevos nombres como nombres de los taxones
+  taxa_names(physeq_objeto) <- nombres_unicos
+  return(physeq_objeto)
+}
+
+# Aplicar renombramiento
+physeq_conocido_filtrado <- renombrar_especies(physeq_conocido_filtrado)
+physeq_todos_filtrado    <- renombrar_especies(physeq_todos_filtrado)
+
+# CONSTRUCCIÓN DE REDES CON SPIEC-EASI
+red_conocido <- spiec.easi(
+  physeq_conocido_filtrado,
   method = "mb",
   lambda.min.ratio = 1e-1,
   nlambda = 20,
@@ -59,8 +78,8 @@ se_known <- spiec.easi(
   pulsar.params = list(thresh = 0.1)
 )
 
-se_all <- spiec.easi(
-  physeq_all_filt,
+red_todos <- spiec.easi(
+  physeq_todos_filtrado,
   method = "mb",
   lambda.min.ratio = 1e-1,
   nlambda = 20,
@@ -68,81 +87,61 @@ se_all <- spiec.easi(
   pulsar.params = list(thresh = 0.1)
 )
 
-g_known <- adj2igraph(getRefit(se_known), vertex.attr = list(name = taxa_names(physeq_known_filt)))
-g_all   <- adj2igraph(getRefit(se_all),   vertex.attr = list(name = taxa_names(physeq_all_filt)))
+# CONVERTIR A OBJETOS igraph
+g_conocido <- adj2igraph(getRefit(red_conocido), vertex.attr = list(name = taxa_names(physeq_conocido_filtrado)))
+g_todos    <- adj2igraph(getRefit(red_todos),    vertex.attr = list(name = taxa_names(physeq_todos_filtrado)))
 
-
-# MÉTRICAS COMPARATIVAS DE LA RED
-
-metricas <- function(g) {
+# FUNCIÓN PARA CALCULAR MÉTRICAS DE LA RED
+calcular_metricas <- function(grafo) {
   list(
-    nodos = vcount(g),  # cuantos nodos
-    aristas = ecount(g),  # Número de aristas
-    grado_medio = mean(degree(g)),  # degree
-    densidad = edge_density(g),  # Densidad de la red
-    clustering = transitivity(g, type = "global"),  # coeficiente de clustering global
-    modularidad = modularity(cluster_louvain(g)),  # Modularidad
-    closeness_media = mean(closeness(g)),  # Promedio de closeness
-    betweenness_media = mean(betweenness(g))  # Promedio de betweenness
+    nodos = vcount(grafo),
+    aristas = ecount(grafo),
+    grado_medio = mean(degree(grafo)),
+    densidad = edge_density(grafo),
+    clustering = transitivity(grafo, type = "global"),
+    modularidad = modularity(cluster_louvain(grafo)),
+    closeness_media = mean(closeness(grafo)),
+    betweenness_media = mean(betweenness(grafo))
   )
 }
 
+metricas_conocido <- calcular_metricas(g_conocido)
+metricas_todos    <- calcular_metricas(g_todos)
 
-m_known <- metricas(g_known)
-m_known
-
-m_all   <- metricas(g_all)
-m_all
-
-# Mostrar comparación
-tibble(
-  Métrica        = names(m_all),
-  Con_materia_obscura = unlist(m_all),
-  Sin_materia_obscura = unlist(m_known)
+# COMPARACIÓN DE MÉTRICAS
+tabla_comparativa <- tibble(
+  Métrica              = names(metricas_todos),
+  Con_materia_obscura  = unlist(metricas_todos),
+  Sin_materia_obscura  = unlist(metricas_conocido)
 )
+print(tabla_comparativa)
 
-
-
-# VISUALIZACIÓN DE g_all (con materia obscura)
-plot(g_all,
-     vertex.size = degree(g_all)*2,
-     vertex.color = cluster_louvain(g_all)$membership,
+# VISUALIZACIÓN DE LAS REDES
+plot(g_todos,
+     vertex.size = degree(g_todos)*2,
+     vertex.color = cluster_louvain(g_todos)$membership,
      vertex.label.cex = 0.7,
      edge.width = 1,
      main = "Red con Materia Obscura")
 
-# VISUALIZACIÓN DE g_known (sin materia obscura)
-plot(g_known,
-     vertex.size = degree(g_known)*2,
-     vertex.color = cluster_louvain(g_known)$membership,
+plot(g_conocido,
+     vertex.size = degree(g_conocido)*2,
+     vertex.color = cluster_louvain(g_conocido)$membership,
      vertex.label.cex = 0.7,
      edge.width = 1,
      main = "Red sin Materia Obscura")
 
+# EXPORTAR A CYTOSCAPE
+createNetworkFromIgraph(g_todos,    title = "Red con materia obscura")
+createNetworkFromIgraph(g_conocido, title = "Red sin materia obscura")
 
-
-createNetworkFromIgraph(g_all,   title = "Red con materia obscura")
-createNetworkFromIgraph(g_known, title = "Red sin materia obscura")
-
-
-
-####################
-#comparacion
-
+# GRAFICAR COMPARACIÓN DE MÉTRICAS CON ggplot2
 library(ggplot2)
 library(tidyr)
 
-# Crear tabla de métricas en formato largo
-df_metricas <- tibble(
-  Métrica        = names(m_all),
-  Con_materia_obscura = unlist(m_all),
-  Sin_materia_obscura = unlist(m_known)
-) %>% pivot_longer(cols = -Métrica, names_to = "Red", values_to = "Valor")
+df_metricas <- tabla_comparativa %>%
+  pivot_longer(cols = -Métrica, names_to = "Red", values_to = "Valor")
 
-# Ver estructura de los datos
-print(df_metricas)
-
-#hacer la grafica con ggplot
 ggplot(df_metricas, aes(x = Métrica, y = Valor, fill = Red)) +
   geom_bar(stat = "identity", position = "dodge") +
   theme_minimal() +
@@ -150,4 +149,4 @@ ggplot(df_metricas, aes(x = Métrica, y = Valor, fill = Red)) +
        x = "Métrica",
        y = "Valor",
        fill = "Tipo de red") +
-  coord_flip() 
+  coord_flip()
